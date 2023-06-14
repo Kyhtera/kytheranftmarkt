b7a7bb2b252f8c5d2167aaca1823dea61cc8258b
Ahmet USLU <ahmetuslum@gmail.com>
2023-06-14 22:03:21 +0300

    Traits correction

--- pages/token/[contractAddress]/[tokenId].tsx
+++ pages/token/[contractAddress]/[tokenId].tsx
@@ -131,18 +131,22 @@ export default function TokenPage({ nft, contractMetadata }: Props) {
 
               <h3 className={styles.descriptionTitle}>Traits</h3>
 
-              <div className={styles.traitsContainer}>
-                {Object.entries(nft?.metadata?.attributes || {}).map(
-                  ([key, value]) => (
+              {Object.entries(nft?.metadata?.attributes || {}).map(
+                ([key, value]) => {
+                  const typedValue = value as {
+                    trait_type: string;
+                    value: string;
+                  };
+                  return (
                     <div className={styles.traitContainer} key={key}>
-                      <p className={styles.traitName}>{key}</p>
-                      <p className={styles.traitValue}>
-                        {value?.toString() || ""}
+                      <p className={styles.traitName}>
+                        {typedValue.trait_type}
                       </p>
+                      <p className={styles.traitValue}>{typedValue.value}</p>
                     </div>
-                  )
-                )}
-              </div>
+                  );
+                }
+              )}
 
               <h3 className={styles.descriptionTitle}>History</h3>
 
